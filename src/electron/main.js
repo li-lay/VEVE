fef18f2d4c1ee68d5dc4feda5570fb5d6aed58e4
LILAY <contact@lilay.dev>
2025-01-20 08:31:58 +0700

    first commit

---
+++ src/electron/main.js
@@ -0,0 +1,35 @@
+import { app, BrowserWindow, globalShortcut } from "electron";
+import path from "path";
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    darkTheme: true,
+    autoHideMenuBar: true,
+  });
+
+  win.loadFile(path.join("dist-react/index.html"));
+
+  // TODO: make a DevTools toggle with globalShortcut F11 -
+  // F12 for toggling the devtools is
+  //  not working properly, so I changed to F11
+  globalShortcut.register("F11", () => {
+    if (win.webContents.isDevToolsOpened()) {
+      win.webContents.closeDevTools();
+      win.setSize(800, 600);
+    } else {
+      win.webContents.openDevTools();
+      win.setSize(1200, 600);
+    }
+  });
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+// Unregister all shortcuts when the app quits
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll();
+});
