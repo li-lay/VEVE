0965a41098679c2559dab8b95b256a69f6378705
LILAY <contact@lilay.dev>
2025-01-20 11:43:02 +0700

    Display node,chrome,electron versions (testing)

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,9 +1,29 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [versions, setVersions] = useState({});
+
+  useEffect(() => {
+    // Access the exposed versions object
+    const nodeVersion = window.versions.node();
+    const chromeVersion = window.versions.chrome();
+    const electronVersion = window.versions.electron();
+
+    // Set the versions in the state
+    setVersions({
+      node: nodeVersion,
+      chrome: chromeVersion,
+      electron: electronVersion,
+    });
+  }, []);
+
   return (
     <>
       <h1>Hello World!</h1>
+      <h2>Nodejs: {versions.node}</h2>
+      <h2>Chrome: {versions.chrome}</h2>
+      <h2>Electron: {versions.electron}</h2>
     </>
   );
 }
