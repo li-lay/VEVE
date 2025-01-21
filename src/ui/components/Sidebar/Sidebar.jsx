9d04179df3915748523c4ade3d444b93c8246c93
LILAY <contact@lilay.dev>
2025-01-21 17:19:24 +0700

    [Sidebar] remove About label / added titles for labels

--- src/ui/components/Sidebar/Sidebar.jsx
+++ src/ui/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,7 @@ const Sidebar = () => {
         className={`border-r border-[#F89B2A]/20 border-solid h-screen w-full ease-in-out duration-500 left-0 shadow-lg shadow-black/15 bg-gray flex flex-col justify-center`}
       >
         <label
+          title="Dashboard"
           htmlFor="dashboard"
           className="relative w-full h-16 p-4 ease-in-out duration-300 border-solid border-black/10 has-[:checked]:border group flex flex-row gap-3 items-center justify-center text-white rounded-xl"
         >
@@ -26,6 +27,7 @@ const Sidebar = () => {
           </svg>
         </label>
         <label
+          title="Options"
           htmlFor="options"
           className="relative w-full h-16 p-4 ease-in-out duration-300 border-solid border-black/10 has-[:checked]:border group flex flex-row gap-3 items-center justify-center text-white rounded-xl"
         >
@@ -51,6 +53,7 @@ const Sidebar = () => {
           </svg>
         </label>
         <label
+          title="Progress"
           htmlFor="progress"
           className="relative w-full h-16 p-4 ease-in-out duration-300 border-solid border-black/10 has-[:checked]:border group flex flex-row gap-3 items-center justify-center text-white rounded-xl"
         >
@@ -71,27 +74,7 @@ const Sidebar = () => {
           </svg>
         </label>
         <label
-          htmlFor="about"
-          className="relative w-full h-16 p-4 ease-in-out duration-300 border-solid border-black/10 has-[:checked]:border group flex flex-row gap-3 items-center justify-center text-white rounded-xl"
-        >
-          <input
-            className="hidden peer/expand"
-            type="radio"
-            name="path"
-            id="about"
-          />
-          <svg
-            className={`fill-current peer-hover/expand:scale-125 peer-hover/expand:text-[#F89B2A] peer-hover/expand:fill-[#F89B2A] peer-checked/expand:text-[#F89B2A] peer-checked/expand:fill-[#F89B2A] text-2xl peer-checked/expand:scale-125 ease-in-out duration-300`}
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-          >
-            <path d="M11.953 2C6.465 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.493 2 11.953 2zM12 20c-4.411 0-8-3.589-8-8s3.567-8 7.953-8C16.391 4 20 7.589 20 12s-3.589 8-8 8z" />
-            <path d="M11 7h2v7h-2zm0 8h2v2h-2z" />
-          </svg>
-        </label>
-        <label
+          title="Settings"
           htmlFor="settings"
           className="relative w-full h-16 p-4 ease-in-out duration-300 border-solid border-black/10 has-[:checked]:border group flex flex-row gap-3 items-center justify-center text-white rounded-xl"
         >
