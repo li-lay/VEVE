eab34a1383192088748baf1c818c2ba80a462413
Samith Seu <59063392+samithseu@users.noreply.github.com>
2025-01-21 18:45:41 +0700

    adding preload events

--- src/electron/preload.mjs
+++ src/electron/preload.mjs
@@ -1,9 +1,10 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("versions", {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
+  closeWin: () => ipcRenderer.send("close-window"),
 });
 
 console.log("Hello Preload!");
