3659c5fdc8b4be3816011cd2fe23f8b1f3069029
LILAY <contact@lilay.dev>
2025-01-22 19:34:00 +0700

    [Settings Page] added Logo

--- src/ui/pages/Settings.jsx
+++ src/ui/pages/Settings.jsx
@@ -1,9 +1,15 @@
 import Wrapper from "../components/Wrapper";
+import VEVELOGO from "../assets/VEVELOGO.png";
 
 const Settings = () => {
   return (
     <Wrapper>
-      <h1>Settings</h1>
+      <div className="fixed left-0 right-0 flex flex-col items-center justify-center gap-2">
+        <div className="flex items-center gap-2">
+          <img className="size-20" src={VEVELOGO} />
+          <h1 className="text-3xl font-bold text-white">VEVE</h1>
+        </div>
+      </div>
     </Wrapper>
   );
 };
