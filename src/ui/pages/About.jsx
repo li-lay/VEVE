c20b73cd0e085eba0c099b5d19dd1f795c8ff364
Samith <samithseu2002@gmail.com>
2025-01-22 10:18:37 +0700

    adding pages

---
+++ src/ui/pages/About.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "../components/Wrapper";
+
+const About = () => {
+  return (
+    <Wrapper>
+      <h1>About</h1>
+    </Wrapper>
+  );
+};
+
+export default About;
---
+++ src/ui/pages/Options.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "../components/Wrapper";
+
+const Options = () => {
+  return (
+    <Wrapper>
+      <h1>Options</h1>
+    </Wrapper>
+  );
+};
+
+export default Options;
