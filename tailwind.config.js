3bd6dd143d7f22f65b566ba4e2b025734053b6b5
LILAY <contact@lilay.dev>
2025-01-22 19:27:48 +0700

    [tailwind] define more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     extend: {
       colors: {
         accent: "#F89B2A",
+        white: "#ffffff",
+        black: "#1f1f1f",
+        bg: "#282828",
+        bg0: "#32302f",
+        fg: "#ebdbb2",
+        fg0: "#fbf1c7",
       },
     },
   },
