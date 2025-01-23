28dfe73b77edbef93e02a378817b3406c1321e90
Samith Seu <59063392+samithseu@users.noreply.github.com>
2025-01-23 07:55:22 +0700

    using padding instead of margin and use svg logo

--- src/ui/components/TitleBar/TitleBar.jsx
+++ src/ui/components/TitleBar/TitleBar.jsx
@@ -1,11 +1,11 @@
-import VEVELOGO from "../../assets/VEVELOGO.png";
+import VEVELOGO from "../../assets/VEVELOGO.svg";
 import CloseIcon from "../icons/CloseIcon";
 
 const TitleBar = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-1 items-center h-8 w-full py-2 bg-accent z-50 fixed top-0 left-0 draggable">
       {/* Title Contents */}
-      <div className="flex justify-start gap-1 w-full col-[1/1] row-[1/1] ml-2">
+      <div className="flex justify-start gap-1 w-full col-[1/1] row-[1/1] ps-4">
         <img src={VEVELOGO} alt="VEVE Logo" className="size-6" />
         <h1 className="text-black font-semibold ">VEVE</h1>
       </div>
