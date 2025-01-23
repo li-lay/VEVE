33437a4172d4ac6dc7cf192640231c77382f6d9c
LILAY <contact@lilay.dev>
2025-01-23 14:50:05 +0700

    [Settings Page] add gaps & create some test settings

--- src/ui/pages/Settings.jsx
+++ src/ui/pages/Settings.jsx
@@ -5,9 +5,28 @@ const Settings = () => {
   return (
     <Wrapper>
       <div className="fixed left-0 right-0 flex flex-col items-center justify-center gap-2">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 mt-6">
           <img className="size-20" src={VEVELOGO} alt="VEVE Logo" />
           <h1 className="text-3xl font-bold text-white">VEVE</h1>
+        </div>
+        <div className="flex mt-6">
+          <ul className="">
+            <li>
+              <button className="w-full cursor-pointer group relative flex gap-1.5 px-8 py-4 bg-black text-white hover:bg-opacity-70 transition font-semibold shadow-md justify-center">
+                Test0
+              </button>
+            </li>
+            <li>
+              <button className="w-full cursor-pointer group relative flex gap-1.5 px-8 py-4 bg-black text-white hover:bg-opacity-70 transition font-semibold shadow-md justify-center">
+                Test1
+              </button>
+            </li>
+            <li>
+              <button className="w-full cursor-pointer group relative flex gap-1.5 px-8 py-4 bg-black text-white hover:bg-opacity-70 transition font-semibold shadow-md justify-center">
+                About
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
     </Wrapper>
