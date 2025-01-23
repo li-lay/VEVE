18736184c6be95bf64a47836035d2122bff170d5
Samith Seu <59063392+samithseu@users.noreply.github.com>
2025-01-23 10:14:55 +0700

    adding maximize/restore icon

--- src/electron/preload.mjs
+++ src/electron/preload.mjs
@@ -6,6 +6,8 @@ contextBridge.exposeInMainWorld("versions", {
   electron: () => process.versions.electron,
   closeWin: () => ipcRenderer.send("close-window"),
   minimizeWin: () => ipcRenderer.send("minimize-window"),
+  RestoreORMaximizeWin: () => ipcRenderer.send("restore-maximize-window"),
+  onWindowStateChange: (callback) => ipcRenderer.on("window-state", callback),
 });
 
 console.log("Hello Preload!");
--- src/ui/components/TitleBar/TitleBar.jsx
+++ src/ui/components/TitleBar/TitleBar.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react";
 import VEVELOGO from "../../assets/VEVELOGO.svg";
-import CloseIcon from "../icons/CloseIcon";
-import HideIcon from "../icons/HideIcon";
 import TitleBarButton from "../TitleBarButton";
+import CloseIcon from "../icons/CloseIcon";
+import MinimizeIcon from "../icons/MinimizeIcon";
+import MaximizeIcon from "../icons/MaximizeIcon";
+import RestoreIcon from "../icons/RestoreIcon";
 
 const TitleBar = () => {
+  const [isMaximized, setIsMaximized] = useState("restored");
+
+  useEffect(() => {
+    window.versions.onWindowStateChange((event, state) => {
+      setIsMaximized(state);
+    });
+
+    return () => {
+      window.versions.onWindowStateChange(() => {});
+    };
+  }, []);
+
   return (
     <div className="grid grid-cols-1 grid-rows-1 items-center h-8 w-full py-2 bg-accent z-50 fixed top-0 left-0 draggable">
       {/* Title Contents */}
@@ -12,14 +27,25 @@ const TitleBar = () => {
         <h1 className="text-black font-semibold ">VEVE</h1>
       </div>
       {/* Buttons */}
-      <div className="w-full col-[1/1] row-[1/1] flex justify-end gap-2 items-center px-4">
+      <div className="w-full col-[1/1] row-[1/1] flex justify-end gap-4 items-center px-4">
         <TitleBarButton
           onClick={() => window.versions.minimizeWin()}
-          icon={<HideIcon className="no-drag text-black size-[22px]" />}
+          icon={<MinimizeIcon className="no-drag text-black size-3.5" />}
         />
+        {isMaximized === "restored" ? (
+          <TitleBarButton
+            onClick={() => window.versions.RestoreORMaximizeWin()}
+            icon={<MaximizeIcon className="no-drag text-black size-3.5" />}
+          />
+        ) : (
+          <TitleBarButton
+            onClick={() => window.versions.RestoreORMaximizeWin()}
+            icon={<RestoreIcon className="no-drag text-black size-3.5" />}
+          />
+        )}
         <TitleBarButton
           onClick={() => window.versions.closeWin()}
-          icon={<CloseIcon className="no-drag text-black size-[22px]" />}
+          icon={<CloseIcon className="no-drag text-black size-3.5" />}
         />
       </div>
     </div>
