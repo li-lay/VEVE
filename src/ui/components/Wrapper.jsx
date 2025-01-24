338d6433d1eeac69dad0ffa6dc94734e509b2006
LILAY <contact@lilay.dev>
2025-01-24 09:06:15 +0700

    [Wrapper] enable scrolls

--- src/ui/components/Wrapper.jsx
+++ src/ui/components/Wrapper.jsx
@@ -1,5 +1,7 @@
 const Wrapper = ({ children }) => {
-  return <div className="pt-12 w-full h-fit ">{children}</div>;
+  return (
+    <div className="pt-12 w-full h-screen overflow-y-auto">{children}</div>
+  );
 };
 
 export default Wrapper;
