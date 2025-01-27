0de9646adfab9880d0d3361b7493c828f0d16d9a
LILAY <contact@lilay.dev>
2025-01-27 18:43:34 +0700

    [Main] Improve error handlings

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,7 +1,6 @@
 import { app, BrowserWindow, globalShortcut } from "electron";
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 import dotenv from "dotenv";
 import {
   setupIPCListeners,
@@ -10,8 +9,11 @@ import {
   cleanupIPCListeners,
 } from "./lib/setups.mjs";
 
-// Get the environment variables
-dotenv.config();
+// Load environment variables
+const result = dotenv.config();
+if (result.error) {
+  throw result.error;
+}
 
 // Set current app mode - "development" | "production"
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
@@ -20,36 +22,41 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Function to create a new browser window
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minimizable: true,
-    minWidth: 600,
-    minHeight: 400,
-    darkTheme: true,
-    autoHideMenuBar: true,
-    frame: false,
-    icon: path.join(__dirname, "../../VEVELOGO.png"),
-    webPreferences: {
-      preload: path.join(__dirname, "preload.mjs"),
-      contextIsolation: true, // Recommended for security
-      enableRemoteModule: false, // Disable remote module
-      nodeIntegration: true,
-    },
-  });
-  setupIPCListeners(win);
-  setupWindowEvents(win);
+  try {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      minimizable: true,
+      minWidth: 600,
+      minHeight: 400,
+      darkTheme: true,
+      autoHideMenuBar: true,
+      frame: false,
+      icon: path.join(__dirname, "../../VEVELOGO.png"),
+      webPreferences: {
+        preload: path.join(__dirname, "preload.mjs"),
+        contextIsolation: true, // Recommended for security
+        enableRemoteModule: false, // Disable remote module
+        nodeIntegration: true,
+      },
+    });
+    setupIPCListeners(win);
+    setupWindowEvents(win);
 
-  // HOT RELOADING
-  if (process.env.NODE_ENV !== "production") {
-    win.loadURL("http://localhost:5173");
-    setupDevToolsShortcut(win);
-  } else {
-    win.loadFile(path.join("dist-react/index.html"));
+    // HOT RELOADING
+    if (process.env.NODE_ENV !== "production") {
+      win.loadURL("http://localhost:5173");
+      setupDevToolsShortcut(win);
+    } else {
+      win.loadFile(path.join("dist-react/index.html"));
+    }
+
+    return win;
+  } catch (error) {
+    console.error("Failed to create the browser window:", error);
   }
-
-  return win;
 };
 
 app.whenReady().then(() => createWindow());
